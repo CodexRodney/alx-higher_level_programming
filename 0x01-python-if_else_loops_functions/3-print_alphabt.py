6f49b56461d45c39ed806eb5afdc0b5c853e2f1c
CodexRodney <rodney002mwanje@gmail.com>
2021-12-14 16:19:46 +0300

    Prints lowercase alphbet except pandq

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 a = 97
-while a <123:
+while a < 123:
     if a == 101 or a == 123:
+        a += 1
         continue
-    print("{}".format(chr(a)),end="")
-    a+=1
+    print("{}".format(chr(a)), end="")
+    a += 1
