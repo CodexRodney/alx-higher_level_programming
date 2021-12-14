8252e2b500c1b58007fdee5e40db0451d61c25a5
CodexRodney <rodney002mwanje@gmail.com>
2021-12-14 17:11:20 +0300

    Prints all the numbers from 0 to 99

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for i in range(0, 10):
     j = i + 1
     while j < 10:
         print("{}{}".format(i, j), end="")
-        if i ==8 and j == 9:
+        if i == 8 and j == 9:
             break
         print(",", end=" ")
         j += 1
