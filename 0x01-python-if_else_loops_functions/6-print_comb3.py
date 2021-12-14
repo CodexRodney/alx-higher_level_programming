10ce88a630e79408c2cc5b133b5902e79a978fda
CodexRodney <rodney002mwanje@gmail.com>
2021-12-14 17:00:23 +0300

    Prints all possible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    j = i + 1
+    while j < 10:
+        print(i, end="")
+        print(j, end="")
+        if i ==8 and j == 9:
+            break
+        print(",", end=" ")
+        j += 1
