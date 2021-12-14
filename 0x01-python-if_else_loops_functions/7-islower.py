efbbc8a2c5b7462c28ddc6e3fa99810b2e83236f
CodexRodney <rodney002mwanje@gmail.com>
2021-12-14 17:21:43 +0300

    A pyhton function that checks whether is lowercase

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def islower(c):
+    """ Checks whether is lowercase
+        @c: Value to be checked
+        """
+        if ord(c) >= 97 and ord <= 22:
+            return True
+        else:
+            return False
