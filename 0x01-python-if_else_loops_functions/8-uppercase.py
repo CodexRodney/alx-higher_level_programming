1f33a72b7ceb0f41e5fd3b37baccd8034ce815ca
CodexRodney <rodney002mwanje@gmail.com>
2021-12-15 01:15:42 +0300

    Python function that returns the last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str)
-    for q in str
+def uppercase(str):
+    for q in str:
         if q >= 97 and q <= 122:
-            q = chr(ord(str[z])-32)
+            q = chr(ord(q)-32)
         print("{}".format(q), end="")
     print("{}".format(""))
