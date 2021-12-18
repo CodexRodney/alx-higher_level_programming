0449104eee33e89080bcee0807d3808b3130c05d
CodexRodney <rodney002mwanje@gmail.com>
2021-12-19 00:13:34 +0300

    Fixed Documentation errors

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,7 +34,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Get/set size of square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -49,7 +48,6 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Gets/sets position of a square"""
         if not len(value) == 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif not isinstance(value[0], int):
