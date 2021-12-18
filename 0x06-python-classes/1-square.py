bd5a83f0ad30de0e42b0589670d385a3b4b99a0e
CodexRodney <rodney002mwanje@gmail.com>
2021-12-18 14:58:02 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+
+This module defines a class called square
+"""
+
+
+class Square:
+    """
+
+    defines a square
+    """
+    def __init__(self,size):
+        """
+        initializes a value size to a class
+
+        Args:
+            self:a reference to an object
+            size: length or height of the square
+        """
+        self.__size = size
