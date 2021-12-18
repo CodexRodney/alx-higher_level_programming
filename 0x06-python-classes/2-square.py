6c602717ec13e5a662af052553e7cfb66f0915de
CodexRodney <rodney002mwanje@gmail.com>
2021-12-18 16:04:06 +0300

    Size Validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -23,24 +23,9 @@ class Square:
             self: a reference to an object
             size(integer:optional): length or height of square
         """
-        self.__size = size
-    @_size.setter
-    def _size(self, size):
-        """
-        validates the size of square
-
-        Args:
-            self: reference to object
-            size(integer): height or length of square
-        """
-        try:
-            if isinstance(size, int) != True:
-                raise TypeError
-            elif size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if isinstance(size, int) != True:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
