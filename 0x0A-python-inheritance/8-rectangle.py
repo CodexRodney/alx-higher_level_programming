227aa112e4620e6ef7219c02908159e33ce47124
CodexRodney <rodney002mwanje@gmail.com>
2021-12-21 04:17:10 +0300

    Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""
-
-defines a subclass Rectangle of BaseGeometry
-"""
-from 7-base_geometry import BaseGeometry
+"""Defines a subclass rectangle from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
