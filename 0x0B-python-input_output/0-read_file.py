efcc5c50b3245f8ce33a78d7a272c771fc60a6d6
CodexRodney <rodney002mwanje@gmail.com>
2021-12-28 14:55:55 +0300

    Corrected code to correct output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Reads a text file and prints it to stdout
     """
     with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
 
