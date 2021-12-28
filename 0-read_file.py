9a4a3ce5884d25176d4e4be4ff946f584265d8d8
CodexRodney <rodney002mwanje@gmail.com>
2021-12-28 14:34:10 +0300

    Read File

--- 0-read_file.py
+++ 0-read_file.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-
-defines a function read_file
-"""
+"""Defines a function read_file."""
 
 
 def read_file(filename=""):
@@ -10,7 +7,7 @@ def read_file(filename=""):
     Reads a text file and prints to stdout
 
     Args:
-        filename: Filename of the file to be read
+        filename(str): Filename of the file to be read
     """
     with open(filename, encoding=UTF8) as myFile:
         print(myFile.read())
