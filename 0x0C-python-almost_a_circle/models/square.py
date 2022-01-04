5dbb5736d3055579a63acfcbc5b7347cfe3a9c3e
CodexRodney <rodney002mwanje@gmail.com>
2022-01-04 16:44:51 +0300

    Updated class till Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,82 +1,77 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+"""
 
-from models.rectangle import Rectangle
+Defines a class Square
+"""
+
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
-
+    """
+    Defines a square
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
+        """
+        Instatiation of values
+
         Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
+            size(int): size of square
+            x = x coordinate of square
+            y = y coordinate of square
+            id = id of square
         """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """
+        Changes string representation of an object
+        """
+        rst = "[Square] ({id}) {x}/{y} - {w}"
+        return rst.format(id=self.id, x=self.x, y=self.y, w=self.width)
+
     @property
     def size(self):
-        """Get/set the size of the Square."""
-        return self.width
+    	"""
+    	Setter/getter of size
+    	"""
+    	return self.width
 
     @size.setter
     def size(self, value):
-        self.width = value
-        self.height = value
+    	if not isinstance(value, int):
+    		raise TypeError("width must be an integer")
+    	elif value <= 0:
+    		raise ValueError("width must be > 0")
+    	else:
+    		self.width = value
+    		self.height = value
 
     def update(self, *args, **kwargs):
-        """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
-        """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
-
-    def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
-        return {
-            "id": self.id,
-            "size": self.width,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def __str__(self):
-        """Return the print() and str() representation of a Square."""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
-                                                 self.width)
+    	"""
+    	Assigns an argument to each list
+    	"""
+    	if len(args) > 0:
+    		for i, val in enumerate(args):
+    			if i == 0:
+    				self.id = val
+    			if i == 1:
+    				self.width = val
+    				self.height = val
+    			if i == 2:
+    				self.x = val
+    			if i == 3:
+    				self.y = val
+    			if i > 3:
+    				break
+    	else:
+    		for key, value in kwargs.items():
+    			if key == 'x':
+    				self.x = value
+    			if key == 'y':
+    				self.y = value
+    			if key == 'size':
+    				self.width = value
+    				self.height = value
+    			if key == 'id':
+    				self.id = value	
