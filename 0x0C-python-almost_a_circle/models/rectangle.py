7fcc25afb65c08f9e83b4842f9d13efe23edf379
CodexRodney <rodney002mwanje@gmail.com>
2022-01-06 13:12:51 +0300

    Updated till Rectangle instance ...

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -146,3 +146,6 @@ class Rectangle(Base):
                     self.width = value
                 if key == 'id':
                     self.id = value
+    def to_dictionary(self):
+        rep_dic = {'x' : self.x, 'y' : self.y, 'id' :  self.id, 'height' : self.height, 'width' : self.width}
+        return rep_dic
