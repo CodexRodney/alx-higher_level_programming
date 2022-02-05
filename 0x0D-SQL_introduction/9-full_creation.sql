dfce6d5863601c49f406d87059c8373df920657f
CodexRodney <rodney002mwanje@gmail.com>
2022-02-05 02:41:38 -0800

    Added  A feature

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
 -- Creates a table in the database hbtn_0c_0
+DROP TABLE IF EXISTS second_table;
 CREATE TABLE second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
