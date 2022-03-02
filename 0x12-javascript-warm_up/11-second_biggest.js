9d3af6f3d110921cfc329abb6dcb4638471026a7
CodexRodney <rodney002mwanje@gmail.com>
2022-03-02 15:23:38 -0800

    Fixed errors on semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-    const arr = process.argv.slice(2);
-    arr.sort((a, b) => b - a);
-	console.log(arr[1]);
+  const arr = process.argv.slice(2);
+  arr.sort((a, b) => b - a);
+  console.log(arr[1]);
 }
