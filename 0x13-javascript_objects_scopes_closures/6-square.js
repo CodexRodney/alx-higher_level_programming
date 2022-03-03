a9d92b64b28f837496b880b11703cd4e879a2035
CodexRodney <rodney002mwanje@gmail.com>
2022-03-02 16:49:29 -0800

    Prits x or shapechar

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (c == undefined) {
+      this.print();
+    } else {
+      let shapechar = '';
+	    for (let i = 0; i < this.width; i++) {
+		    shapechar += String(c);
+	    }
+	    for (let z = 0; z < this.height; z++) {
+		    console.log(shapechar);
+	    };
+    };
+  }
+};
