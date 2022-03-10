dd32103419116fa76b62594eb9ec1f11e7524a10
CodexRodney <rodney002mwanje@gmail.com>
2022-03-10 11:42:58 +0300

    Cities y states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,19 @@
+from sys import argv
+import MySQLdb
+
+
+def main():
+    """
+    runs as the main fuction
+    """
+    db = MySQLdb.connect(host=argv[2], passwd=argv[3], db=argv[4])
+    sql = "SELECT name FROM cities  ORDER BY id ASC;"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for x in results:
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
