e9111b5b04101bc40c24ccf988cbc223efd1f7ff
CodexRodney <rodney002mwanje@gmail.com>
2022-03-19 01:13:47 +0300

    Response header value #0

---
+++ 0x11-python-netwotk_1/1-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Takes in a URL sends a request and displays
+value of X-Request_Id variable
+"""
+
+import urllib.request
+import sys
+
+def main():
+    url = str(sys.argv[1])
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        header = response.headers
+        print(header['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    main()
