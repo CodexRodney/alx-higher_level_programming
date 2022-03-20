412e8a9678620a92935568a923f52e6ee59b2d35
CodexRodney <rodney002mwanje@gmail.com>
2022-03-20 10:41:08 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#takes in a uRL sends a request to the url, prints the 
-#size of the ody of the response
+#takes in a uRL sends a request to the url
 
 curl -sD - "$1" | grep "Content-Length" | cut -d " " -f 2-
