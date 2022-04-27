56fa281351fa692ce8e9963b565fb421322f6c88
CodexRodney <rodney002mwanje@gmail.com>
2022-04-27 23:11:24 +0300

    Changed a file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-for x in dir(hidden_4):
-    if x[0] == "_" and x[1] == "_":
-        continue
+if __name__ == "__main":
+    for x in dir(hidden_4):
+        if x[0] == "_" and x[1] == "_":
+            continue
     print(x)
