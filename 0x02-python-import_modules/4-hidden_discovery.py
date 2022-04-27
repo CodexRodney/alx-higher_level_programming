2fe39c262669d23d904a6667b76b5f4014ae1171
CodexRodney <rodney002mwanje@gmail.com>
2022-04-27 23:05:53 +0300

    Who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import hidden_4
-
-if __name__ == '__main__':
-    def vdir(hidden_4):
-        return [x for x in dir(hidden_4) if not x.startswith('__')]
-    print('\n'.join(str(x) for x in vdir(hidden_4)))
+for x in dir(hidden_4):
+    if x[0] == "_" and x[1] == "_":
+        continue
+    print(x)
