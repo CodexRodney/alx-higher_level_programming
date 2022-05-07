22ddb1760b134bed3424e22da54b76482af4e31e
CodexRodney <rodney002mwanje@gmail.com>
2022-05-07 12:14:14 +0300

    Load,add,save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list"""
+"""
+Adds all arguments to a python list then saves them to a file
+"""
+
 
 import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+def append_to_list(list1):
+    """
+    Adds items to a list
+
+    Args:
+        list1: list in which itmes are added
+    """
+    for i, x in enumerate(sys.argv):
+        if i == 0:
+            continue
+        list1.append(x)
+
+
+def main():
+    """
+    Runs only if __name__ == "__main__"
+    """
+    filename = "add_item.json"
+    arg_list = []
+    try:
+        arg_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        append_to_list(arg_list)
+        save_to_json_file(arg_list, filename)
+    else:
+        append_to_list(arg_list)
+        save_to_json_file(arg_list, filename)
+
 
 if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-
-    try:
-    s_args = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        s_args = []
-
-    for i in sys.argv:
-        s_args.append(i)
-
-    s_args.pop(0)
-    save_to_json_file(s_args, "add_item.json")
+    main()
