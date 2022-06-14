c53d07002f6a6a4d713c156d67608f086fd8bc40
CodexRodney <rodney002mwanje@gmail.com>
2022-06-14 16:39:52 +0300

    Fixed pycodestyle errors

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,11 +8,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
-# engine = create_engine('mysql://root@localhost:3306')
+
 Base = declarative_base()
 
 
 class State(Base):
+    """
+    Model for states table
+    """
     __tablename__ = "states"
-    id = Column(Integer, primary_key = True)
-    name = Column(String(128), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
