bdae16c30caaf5ea6ecd01980e5cb9c9fb7f041d
CodexRodney <rodney002mwanje@gmail.com>
2022-06-15 01:53:29 +0300

    delete query

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Deletes all State objects from the database hbtn_0e_6_usa
+which contain the letter a
+"""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy import delete
+
+
+if __name__ == "__main__":
+    str1 = 'mysql://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3])
+    engine = create_engine(str1)
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    results = session.query(State).filter(State.name.like('%a%')).delete(synchronize_session=False)
+    session.commit()
