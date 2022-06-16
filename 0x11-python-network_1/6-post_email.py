81ccf51c922e33b5e38edb077616da2be4ade29a
CodexRodney <rodney002mwanje@gmail.com>
2022-06-16 21:02:07 +0300

    Hot fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -17,7 +17,7 @@ def main():
     email = str(sys.argv[2])
     header = {'email': email}
     res = requests.post(url, data=header)
-    print("email: {}".format(res.text))
+    print(res.text["email"])
 
 
 if __name__ == "__main__":
