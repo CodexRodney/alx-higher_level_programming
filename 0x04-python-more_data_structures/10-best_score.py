0f5c136de28ab19afdac5022c056031f0f526027
CodexRodney <rodney002mwanje@gmail.com>
2022-06-18 15:00:38 +0300

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if len(a_dictionary) == 0 or a_dictionary is dict:
+    if not type(a_dictionary) is dict:
+        return None
+
+    if len(a_dictionary) == 0:
         return None
 
     retval = list(a_dictionary.keys())[0]
     bigval = a_dictionary[retval]
     for k, v in a_dictionary.items():
-        if v > big:
+        if v > bigval:
             bigval = v
             retval = k
     return retval
