75d53d35b508e08c5a2dddf1da39e2602e1d2cd9
CodexRodney <rodney002mwanje@gmail.com>
2022-06-18 16:05:22 +0300

    Hot Fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,19 +3,21 @@ def safe_print_list_integers(my_list=[], x=0):
     numitems = 0
     for z in my_list:
         numitems += 1
-    if x > numitems:
-        raise IndexError
+
     num = 0
 
     new_list = []
     try:
         new_list = my_list[:x]
         for i in new_list:
-            if isinstance(i, int) != True:
+            if not isinstance(i, int):
                 continue
             print("{:d}".format(i), end="")
             num += 1
-        print()
-        return num
+        if x > numitems:
+            raise IndexError
     except IndexError:
         raise
+    else:
+        print()
+        return num
