97084df9407fa87a8b626c45d85227d7b96ab06c
CodexRodney <rodney002mwanje@gmail.com>
2022-06-18 15:19:51 +0300

    fixed pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x = 0):
+def safe_print_list(my_list=[], x=0):
     num = 0
     new_list = []
     try:
@@ -9,5 +9,5 @@ def safe_print_list(my_list=[], x = 0):
             num += 1
         print()
         return num
-    except:
+    except Exception as e:
         print("Invalid Syntax")
