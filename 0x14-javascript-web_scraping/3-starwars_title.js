f1353c768d57d8e3a10018999a4ce8a135d876f6
CodexRodney <rodney002mwanje@gmail.com>
2022-07-19 13:20:55 +0300

    Added file 3-starwars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+/* Prints the title of a Star Wars movie where
+ * the episode number matches a given integer
+ * which is given in the command line
+ */
+
+const request = require('request');
+
+// url to the end point used
+// url should be https://swapi-api.hbtn.io/api/films/:id
+// (id) provided in command line
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) console.log(error);
+  const results = JSON.parse(body); // holds the body of response
+  console.log(results.title);
+});
