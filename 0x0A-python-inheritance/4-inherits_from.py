6415488387ab184a589ea16acef4b49249ab8621
CodexRodney <rodney002mwanje@gmail.com>
2022-07-21 10:21:58 +0300

    Made file executable

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
     Checks whether an obj is a kind of an instance
     of the specified type
     """
-    if isinstance(type(obj), a_class) and type(obj) != a_class:
+    if isinstance(obj, a_class) and not type(obj) is a_class:
         return True
     else:
         return False
