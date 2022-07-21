ae37408b232f558446df10487a17790d244cbcbb
CodexRodney <rodney002mwanje@gmail.com>
2022-07-21 14:48:44 +0300

    Added file 100-my

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines a class Myint
+"""
+
+class MyInt(int):
+    """
+    Inherits from int
+    """
+    def __eq__(self, other):
+        """
+        Inverts the == operator to !=
+        """
+        return self.real != other
+        
+
+    def __ne__(self, other):
+        """
+        Inverts the != operator to ==
+        """
+        return self.real == other
